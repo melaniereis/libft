e9f3b11af8b10b845445143517ac41b92c8a8a28
Melanie Ferraz Reis <meferraz@c1r2s5.42porto.com>
2024-12-19 16:05:07 +0000

    fix include path

--- srcs_extra/ft_strjoin_free.c
+++ srcs_extra/ft_strjoin_free.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "./libft.h"
+#include "../includes/libft.h"
 
 char	*ft_strjoin_free(char const *s1, char const *s2)
 {
